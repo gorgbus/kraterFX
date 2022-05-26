c2509e57d5586c147717d2a05c05f22932022e97
gorgbus <63368174+gorgbus@users.noreply.github.com>
2022-05-26 22:50:38 +0200

    nav update

--- components/navbar.js
+++ components/navbar.js
@@ -9,10 +9,17 @@ class Navbar extends HTMLElement {
                 <div class="shadow"></div>
 
                 <ul>
-                    <li class="logo"><a href="index.html">krater<span>FX</span></a></li>
-                    <li><a href="kontakt.html">Kontakt</a></li>
-                    <li><a href="produkty.html">Produkty</a></li>
-                    <li><a href="o_nas.html">O nás</a></li>
+                    <li class="logo"><a href="./index.html">krater<span>FX</span></a></li>
+                    <li><a href="./kontakt.html">Kontakt</a></li>
+                    <li>
+                        <a href="./produkty.html">Produkty</a>
+
+                        <ul>
+                            <li><a href="./movx.html">movX Pro</a></li>
+                            <li><a href="./paintx.html">paintX Pro</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="./o_nas.html">O nás</a></li>
                 </ul>
             </nav>
         `;
